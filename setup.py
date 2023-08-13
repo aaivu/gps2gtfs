6d925954a696fc46f6aeabf701df98371a261be2
shangopi <gopinath.19@cse.mrt.ac.lk>
2023-08-13 16:19:19 +0530

    release files updated

--- setup.py
+++ setup.py
@@ -4,23 +4,28 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 classifiers = [
+    'Development Status :: 3 - Alpha',
     "Programming Language :: Python :: 3",
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
     "License :: OSI Approved :: MIT License",
     "Operating System :: OS Independent",
 ]
 
 setup(
     name='gps2gtfs',
+    packages=['gps2gtfs'],
     version='0.0.1',
     description='Extracting travel time information from Bus GPS raw data',
     long_description=long_description,
-    url='',
+    url='https://github.com/aaivu/gps2gtfs',
+    keywords=['gtfs', 'GPS', 'travel time', 'bus trave time'],  # Keywords that define your package best
     author='AAIVU',
-    author_email='',
+    author_email='helloaaivu@gmail.com',
     license='MIT',
     classifiers=classifiers,
     package_dir={"": "src"},
-    packages=find_packages(where="src"),
     python_requires=">=3.6",
-    install_requires=['pandas', 'geopandas']
+    install_requires=['pandas', 'geopandas', 'numpy']
 )
